7a50916285a637eb45307b4bdd5e87b6f5db9a6d
kahenyamercy <kahenyamercy5@gmail.com>
2024-03-07 01:39:01 +0300

    updat the file to remove the whitespaces

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,5 +1,9 @@
+#!/usr/bin/python3
+"""pascal's triangle"""
+
+
 def pascal_triangle(n):
-    """Return a list of lists representing Pascal's triangle up to the nth row."""
+    """Return a list representing Pascal's triangle upto nth row"""
     if n <= 0:
         return []
 
@@ -7,7 +11,7 @@ def pascal_triangle(n):
     for i in range(n):
         row = [1]  # First element of each row is always 1
         for j in range(1, i):
-            # Each element (except the first and last) is the sum of the two elements above it
+            # Each element(except first last)is the sum of two elements aboveit
             row.append(triangle[i-1][j-1] + triangle[i-1][j])
         if i > 0:
             row.append(1)  # Last element of each row is always 1
