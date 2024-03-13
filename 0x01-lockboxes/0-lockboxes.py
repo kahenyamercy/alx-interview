175f55ce1709cef0380967bcfac16ed672b595e1
kahenyamercy <kahenyamercy5@gmail.com>
2024-03-13 20:29:37 +0300

    lock box challenge

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,18 +1,26 @@
 #!/usr/bin/python3
-#lockboxes
+
 def canUnlockAll(boxes):
+    """
+    Determines if all the boxes can be opened.
+    
+    Args:
+        boxes (list of lists): A list of lists representing boxes and their keys.
+        
+    Returns:
+        bool: True if all boxes can be opened, False otherwise.
+    """
     n = len(boxes)
-    visited = set()
-    stack = [0]  # Start with the first box
-
-    while stack:
-        current_box = stack.pop()
-        visited.add(current_box)
-
-        # Push all keys from the current box onto the stack
-        for key in boxes[current_box]:
-            if key < n and key not in visited:
-                stack.append(key)
-
-    # Check if all boxes are visited
-    return len(visited) == n
+    visited = set()  # Set to store visited boxes
+    
+    # Recursive function to explore boxes
+    def dfs(box_index):
+        visited.add(box_index)  # Mark the box as visited
+        keys = boxes[box_index]  # Get keys in the current box
+        for key in keys:
+            if key not in visited and key < n:
+                dfs(key)  # Explore the key
+        
+    dfs(0)  # Start DFS from the first box
+    
+    return len(visited) == n  # Check if all boxes are visited
