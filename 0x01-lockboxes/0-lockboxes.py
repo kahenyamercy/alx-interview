59b4a3a2d0848a25808004a5748536c97cf7cbc6
kahenyamercy <kahenyamercy5@gmail.com>
2024-03-13 20:41:05 +0300

    fix pycodestyle test

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -5,16 +5,16 @@
 def canUnlockAll(boxes):
     """
     Determines if all the boxes can be opened.
-    
+
     Args:
-        boxes (list of lists): A list of lists representing boxes and their keys.
-        
+        boxes (list of lists):list of lists representing boxes and their keys.
+
     Returns:
         bool: True if all boxes can be opened, False otherwise.
     """
     n = len(boxes)
     visited = set()  # Set to store visited boxes
-    
+
     # Recursive function to explore boxes
     def dfs(box_index):
         visited.add(box_index)  # Mark the box as visited
@@ -22,7 +22,7 @@ def canUnlockAll(boxes):
         for key in keys:
             if key not in visited and key < n:
                 dfs(key)  # Explore the key
-        
+
     dfs(0)  # Start DFS from the first box
-    
+
     return len(visited) == n  # Check if all boxes are visited
