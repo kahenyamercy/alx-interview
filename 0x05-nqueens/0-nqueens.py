ccdbb4f8df88ffdeb1934e6f2df2aec77b13841f
kahenyamercy <kahenyamercy5@gmail.com>
2024-04-09 11:21:07 +0300

    backtracking nqueens puzzle

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -3,10 +3,10 @@
 N Queens Problem Solver
 
 Usage: nqueens.py N
-  - N: An integer rep size of chessboard and number of queens.
+  - N: An integer rep size of the chessboard and the number of queens.
 
-Prints every possible sol to the N Queens problem, one sol per line
-Each sol is rep as a list of queen positions (row, column).
+Prints every possible solution to the problem, one solution per line.
+Each solution is represented as a list of queen positions (row, column).
 """
 import sys
 
@@ -15,8 +15,8 @@ def is_safe(board, row, col, N):
     """Check if it's safe to place a queen at the given position (row, col)."""
     for i in range(row):
         if board[i][col] == 1 or \
-            board[i][col-row+i] == 1 or \
-                board[i][col+row-i] == 1:
+            (col-row+i >= 0 and board[i][col-row+i] == 1) or \
+            (col+row-i < N and board[i][col+row-i] == 1):
             return False
     return True
 
@@ -36,15 +36,25 @@ def solve_n_queens_util(board, row, N, result):
 
 def solve_n_queens(N):
     """Solve the N Queens problem and print the solutions."""
-    if not N.isdigit():
+    try:
+        N = int(N)
+    except ValueError:
         print("N must be a number")
         sys.exit(1)
-    N = int(N)
+
     if N < 4:
         print("N must be at least 4")
         sys.exit(1)
+
     board = [[0 for _ in range(N)] for _ in range(N)]
     result = []
     solve_n_queens_util(board, 0, N, result)
     for solution in result:
         print(solution)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    solve_n_queens(sys.argv[1])
