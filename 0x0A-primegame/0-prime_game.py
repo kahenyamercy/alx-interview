e092d120a7d7b5b67650785a28ec6942284faec9
kahenyamercy <kahenyamercy5@gmail.com>
2024-05-15 01:55:21 +0300

    Prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,36 +1,35 @@
 #!/usr/bin/python3
-""" Prime Game """
-
-
-def is_prime(num):
-    """
-    Check if a number is prime.
-    """
-    if num <= 1:
-        return False
-    for i in range(2, int(num ** 0.5) + 1):
-        if num % i == 0:
-            return False
-    return True
-
 
 def isWinner(x, nums):
-    """
-    Determine the winner of the game.
-    """
-    maria_wins = 0
-    ben_wins = 0
+    def is_prime(num):
+        if num < 2:
+            return False
+        for i in range(2, int(num ** 0.5) + 1):
+            if num % i == 0:
+                return False
+        return True
 
-    for n in nums:
-        primes = [num for num in range(2, n+1) if is_prime(num)]
+    def get_primes(n):
+        primes = []
+        for i in range(2, n + 1):
+            if is_prime(i):
+                primes.append(i)
+        return primes
+
+    def game_winner(n):
+        primes = get_primes(n)
         if len(primes) % 2 == 0:
-            ben_wins += 1
+            return "Ben"
         else:
-            maria_wins += 1
+            return "Maria"
 
-    if maria_wins == ben_wins:
-        return None
-    elif maria_wins > ben_wins:
+    winners = [game_winner(num) for num in nums]
+    maria_wins = winners.count("Maria")
+    ben_wins = winners.count("Ben")
+
+    if maria_wins > ben_wins:
         return "Maria"
-    else:
+    elif ben_wins > maria_wins:
         return "Ben"
+    else:
+        return None
